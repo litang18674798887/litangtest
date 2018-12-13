43a6c15f6f987bff64f69fbd6fa430dac7a74416
litang1225 <673829817@qq.com>
2018-12-13 23:57:23 +0800

    111

---
+++ src/main/java/com/lt/litangtest/LitangTestApplication.java
@@ -0,0 +1,14 @@
+package com.lt.litangtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitangTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitangTestApplication.class, args);
+    }
+
+}
+
